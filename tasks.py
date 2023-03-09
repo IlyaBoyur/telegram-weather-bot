d782d278b380c7f476f7c3581758fac53cbb29e9
Ilya Boyur <i.boyur@idaproject.com>
2023-03-09 22:12:20 +0300

    Move data calculation in DataCalculationTask

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ import logging
 from concurrent.futures import ProcessPoolExecutor
 from constants import ERROR_WEATHER_API, TIMEOUT_PERIOD
 from utils import CITIES, ERR_MESSAGE_TEMPLATE
-
+from constants import FORECAST_TARGET_HOURS, PLEASANT_CONDITIONS
 
 logger = logging.getLogger(__name__)
 
@@ -32,7 +32,49 @@ class DataFetchingTask:
 class DataCalculationTask:
     """Вычисление погодных параметров"""
 
-    pass
+    def __init__(self, data):
+        self.data = data
+        self.unique_days = set()
+        self.city_forecasts = {}
+
+    @staticmethod
+    def select_forecast_hours(forecast):
+        hours = [
+            hour for hour in forecast["hours"] if int(hour["hour"]) in FORECAST_TARGET_HOURS
+        ]
+        return hours
+
+    @staticmethod
+    def calculate_avg_temperature(forecast_hours):
+        """Считает среднюю температуру за период"""
+
+        temperature = sum(hour["temp"] for hour in forecast_hours) / len(forecast_hours)
+        return temperature
+    
+    @staticmethod
+    def calculate_comfort_hours(forecast_hours):
+        """Считает время без осадков за период"""
+
+        pleasant_hours = sum(
+            1 for hour in forecast_hours if hour["condition"] in PLEASANT_CONDITIONS
+        )
+        return pleasant_hours
+
+    def worker(self):
+        for city, data in self.data:
+            forecast_days = {}
+            
+            for forecast in data["forecasts"]:
+                hours = self.select_forecast_hours(forecast)
+                if not hours:
+                    continue
+                forecast_days[forecast["date"]] = {
+                    "temperature_avg": self.calculate_avg_temperature(hours),
+                    "pleasant_hours": self.calculate_comfort_hours(hours),
+                }
+
+            self.city_forecasts[city] = {"forecast_days": forecast_days}
+            self.unique_days.update(forecast_days.keys())
 
 
 class DataAggregationTask:
