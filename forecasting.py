e5efdbae2364fedfe8ab59922ed9636939ff8124
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-03-09 20:13:15 +0300

    Add simple non parallel solution

--- forecasting.py
+++ forecasting.py
@@ -1,27 +1,138 @@
-# import logging
+import logging
+
 # import threading
 # import subprocess
-# import multiprocessing
+import multiprocessing
+from datetime import datetime
 
 from api_client import YandexWeatherAPI
-from tasks import (
-    DataFetchingTask,
-    DataCalculationTask,
-    DataAggregationTask,
-    DataAnalyzingTask,
-)
-from utils import CITIES
+from tasks import DataAggregationTask, DataAnalyzingTask, DataCalculationTask, DataFetchingTask
+from utils import CITIES, FORECAST_TARGET_HOURS, PLEASANT_CONDITIONS
+
+logger = logging.getLogger()
 
 
 def forecast_weather():
-    """
-    Анализ погодных условий по городам
-    """
-    # city_name = "MOSCOW"
-    # ywAPI = YandexWeatherAPI()
-    # resp = ywAPI.get_forecasting(city_name)
-    pass
+    """Анализ погодных условий по городам"""
+
+    # data request
+    with multiprocessing.Pool() as pool:
+        rough_data = pool.map(DataFetchingTask(YandexWeatherAPI()).run, CITIES)
+
+    # weather params
+    unique_days = set()
+    city_forecasts = {}
+
+    for city, data in rough_data:
+        forecast_days = {}
+        for forecast in data["forecasts"]:
+            forecast_hours = [
+                hour for hour in forecast["hours"] if int(hour["hour"]) in FORECAST_TARGET_HOURS
+            ]
+            if not forecast_hours:
+                continue
+
+            # average temperature
+            temperature_avg = sum(hour["temp"] for hour in forecast_hours) / len(forecast_hours)
+            # pleasant condition hours
+            pleasant_hours = sum(
+                1 for hour in forecast_hours if hour["condition"] in PLEASANT_CONDITIONS
+            )
+
+            forecast_days[forecast["date"]] = {
+                "temperature_avg": temperature_avg,
+                "pleasant_hours": pleasant_hours,
+            }
+
+        city_forecasts[city] = {"forecast_days": forecast_days}
+        unique_days.update(forecast_days.keys())
+
+    unique_city_names = set(city_forecasts.keys())
+    logger.info(f"city_forecasts: {city_forecasts}")
+
+    # # aggregations
+    city_aggregations = {}
+
+    for city in city_forecasts:
+        days = city_forecasts[city]["forecast_days"]
+        temperature_total_avg = sum(days[day]["temperature_avg"] for day in days) / len(days)
+        hours_total_avg = sum(days[day]["pleasant_hours"] for day in days) / len(days)
+
+        city_aggregations[city] = {
+            "temperature_total_avg": temperature_total_avg,
+            "hours_total_avg": hours_total_avg,
+        }
+
+    # rating
+    city_ratings = {}
+
+    for number, city in enumerate(
+        sorted(
+            city_aggregations,
+            key=lambda city: (
+                -city_aggregations[city]["temperature_total_avg"],
+                -city_aggregations[city]["hours_total_avg"],
+            ),
+        ),
+        1,
+    ):
+        logger.info(
+            f"{number:3}) {city:15}"
+            f"   temp_avg:{city_aggregations[city]['temperature_total_avg']:5.2f}"
+            f"   hours_avg:{city_aggregations[city]['hours_total_avg']:5.2f}"
+        )
+        city_ratings[city] = {"rating": number}
+
+    # export
+    sorted_days = sorted(unique_days)
+
+    logger.info(f"unique days: {sorted_days}")
+    import tablib
+
+    dataset = tablib.Dataset()
+    dataset.headers = [
+        "Город/день",
+        "",
+        *[datetime.strptime(day, "%Y-%m-%d").strftime("%d-%m") for day in sorted_days],
+        "Среднее",
+        "Рейтинг",
+    ]
+
+    for city in unique_city_names:
+        days = city_forecasts[city]["forecast_days"]
+        temperature_avg_days = [days.get(day, {}).get("temperature_avg", "") for day in sorted_days]
+        dataset.append(
+            [
+                city.lower().title(),
+                "Температура, среднее",
+                *[
+                    f"{temperature:.1f}" if temperature else ""
+                    for temperature in temperature_avg_days
+                ],
+                f"{city_aggregations[city]['temperature_total_avg']:.1f}",
+                city_ratings[city]["rating"],
+            ]
+        )
+
+        pleasant_hours_avg_days = [
+            days.get(day, {}).get("pleasant_hours", "") for day in sorted_days
+        ]
+        dataset.append(
+            [
+                "",
+                "Без осадков, часов",
+                *[f"{hours:.1f}" if hours else "" for hours in pleasant_hours_avg_days],
+                f"{city_aggregations[city]['hours_total_avg']:.1f}",
+                "",
+            ]
+        )
+
+    # Write spreadsheet to disk
+    with open("forecasts.xls", "wb") as f:
+        f.write(dataset.export("xls"))
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="[%(levelname)s] - %(asctime)s - %(message)s", level=logging.INFO)
+
     forecast_weather()
--- tasks.py
+++ tasks.py
@@ -1,14 +1,30 @@
+from utils import ERR_MESSAGE_TEMPLATE
+
+
 class DataFetchingTask:
-    pass
+    """Получение данных через API"""
+
+    def __init__(self, api):
+        self.api = api
+
+    def run(self, city):
+        try:
+            return city, self.api.get_forecasting(city)
+        except Exception as error:
+            logger.error(ERR_MESSAGE_TEMPLATE)
 
 
 class DataCalculationTask:
+    """Вычисление погодных параметров"""
+
     pass
 
 
 class DataAggregationTask:
+    """Объединение вычисленных данных"""
     pass
 
 
 class DataAnalyzingTask:
+    """Финальный анализ и получение результата"""
     pass
