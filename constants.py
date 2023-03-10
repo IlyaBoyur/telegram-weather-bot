c570d5f4be7ac43bdaa4a6b07586697cec48cea8
Ilya Boyur <i.boyur@idaproject.com>
2023-03-10 17:38:55 +0300

    Fix formatting

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
-ERROR_WEATHER_API = "Возникла ошибка {error} при запросе в API Яндекс Погоды. Город:{city}"
+ERROR_WEATHER_API = (
+    "Возникла ошибка {error} при запросе в API Яндекс Погоды. Город:{city}"
+)
 TIMEOUT_PERIOD = 25
 PLEASANT_CONDITIONS = set(["clear", "partly-cloudy", "cloudy", "overcast"])
 FORECAST_TARGET_HOURS = set(range(9, 20))
