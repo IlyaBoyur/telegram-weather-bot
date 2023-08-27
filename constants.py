4043cd9af80122c60debb1ad2562bbea5574c630
Ilya Boyur <ilya.boyur.r@yandex.ru>
2023-08-27 12:42:01 +0300

    Simplify constants

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ ERROR_WEATHER_API = (
     "Возникла ошибка {error} при запросе в API Яндекс Погоды. Город:{city}"
 )
 TIMEOUT_PERIOD = 25
-PLEASANT_CONDITIONS = set(["clear", "partly-cloudy", "cloudy", "overcast"])
+PLEASANT_CONDITIONS = {"clear", "partly-cloudy", "cloudy", "overcast"}
 FORECAST_TARGET_HOURS = set(range(9, 20))
