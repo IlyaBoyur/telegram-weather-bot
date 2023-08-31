779bd9468ba7bdee21876de01fafea53098c3927
Ilya Boyur <ilya.boyur.r@yandex.ru>
2023-08-31 20:02:04 +0300

    Move task processing to separate function

--- forecasting.py
+++ forecasting.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Dict, List, Tuple, Type
 
 from api_client import YandexGeoAPI, YandexWeatherAPI
 from tasks import (
@@ -8,39 +9,46 @@ from tasks import (
     DataFetchingTask,
     GeoDataFetchingTask,
     GeoDataParsingTask,
+    Task,
 )
 from utils import CITIES
 
 logger = logging.getLogger()
 
 
-def forecast_weather():
+def process_tasks(tasks: List[Tuple[Type[Task], Dict[str, Any]]]):
     """
-    Анализ погодных условий по городам.
     "_input" в списке параметров -
     куда направить результат работы предыдущей задачи.
     Значение None означает, что входные данные не требуются.
     """
-    for task, params in [
-        [
+    for task, params in tasks:
+        if (parameter := params.pop("_input", None)) is not None:
+            params = {f"{parameter}": data, **params}
+        data = task(**params).worker()
+    return data
+
+
+def forecast_weather():
+    """
+    Анализ погодных условий по городам.
+    """
+    tasks = [
+        (
             GeoDataFetchingTask,
             {
                 "api": YandexGeoAPI(),
                 "addresses": CITIES.keys(),
                 "_input": None,
             },
-        ],
-        [GeoDataParsingTask, {"_input": "locations"}],
-        [DataFetchingTask, {"api": YandexWeatherAPI(), "_input": "locations"}],
-        [DataCalculationTask, {"_input": "forecasts"}],
-        [DataAggregationTask, {"_input": "city_aggregations"}],
-        [DataAnalyzingTask, {"_input": "cities"}],
-    ]:
-        if (parameter := params.pop("_input", None)) is not None:
-            params = {f"{parameter}": data, **params}
-        data = task(**params).worker()
-
-    return data
+        ),
+        (GeoDataParsingTask, {"_input": "locations"}),
+        (DataFetchingTask, {"api": YandexWeatherAPI(), "_input": "locations"}),
+        (DataCalculationTask, {"_input": "forecasts"}),
+        (DataAggregationTask, {"_input": "city_aggregations"}),
+        (DataAnalyzingTask, {"_input": "cities"}),
+    ]
+    return process_tasks(tasks)
 
 
 if __name__ == "__main__":
