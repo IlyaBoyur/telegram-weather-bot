21d4ee31283af95c3f5cea175dbe8d686db1490b
Ilya Boyur <ilya.boyur.r@yandex.ru>
2023-08-31 19:16:54 +0300

    Add constants

--- constants.py
+++ constants.py
@@ -1,5 +1,13 @@
 ERROR_WEATHER_API = (
-    "Возникла ошибка {error} при запросе в API Яндекс Погоды. Город:{city}"
+    "Возникла ошибка {error} при запросе в API Яндекс Погоды. Город: {city}"
+)
+ERROR_GEO_API = (
+    "Возникла ошибка {error}"
+    " при запросе в API Яндекс Геокодера. Адрес: {address}"
+)
+ERROR_GEO_PARSING_API = (
+    "Возникла ошибка {error}"
+    " при обработке ответа API Яндекс Геокодера. Адрес: {address}"
 )
 TIMEOUT_PERIOD = 25
 PLEASANT_CONDITIONS = {"clear", "partly-cloudy", "cloudy", "overcast"}
