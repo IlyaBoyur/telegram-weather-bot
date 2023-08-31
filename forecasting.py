34861686a67be157dcd05e17652ce55d04e3c838
Ilya Boyur <ilya.boyur.r@yandex.ru>
2023-08-31 19:42:33 +0300

    Add multi purpose task handler

--- forecasting.py
+++ forecasting.py
@@ -1,26 +1,45 @@
 import logging
 
-from api_client import YandexWeatherAPI
+from api_client import YandexGeoAPI, YandexWeatherAPI
 from tasks import (
     DataAggregationTask,
     DataAnalyzingTask,
     DataCalculationTask,
     DataFetchingTask,
+    GeoDataFetchingTask,
+    GeoDataParsingTask,
 )
+from utils import CITIES
 
 logger = logging.getLogger()
 
 
 def forecast_weather():
-    """Анализ погодных условий по городам."""
-    data = YandexWeatherAPI()
-    for task in [
-        DataFetchingTask,
-        DataCalculationTask,
-        DataAggregationTask,
-        DataAnalyzingTask,
+    """
+    Анализ погодных условий по городам.
+    "_input" в списке параметров -
+    куда направить результат работы предыдущей задачи.
+    Значение None означает, что входные данные не требуются.
+    """
+    for task, params in [
+        [
+            GeoDataFetchingTask,
+            {
+                "api": YandexGeoAPI(),
+                "addresses": CITIES.keys(),
+                "_input": None,
+            },
+        ],
+        [GeoDataParsingTask, {"_input": "locations"}],
+        [DataFetchingTask, {"api": YandexWeatherAPI(), "_input": "locations"}],
+        [DataCalculationTask, {"_input": "forecasts"}],
+        [DataAggregationTask, {"_input": "city_aggregations"}],
+        [DataAnalyzingTask, {"_input": "cities"}],
     ]:
-        data = task(data).worker()
+        if (parameter := params.pop("_input", None)) is not None:
+            params = {f"{parameter}": data, **params}
+        data = task(**params).worker()
+
     return data
 
 
