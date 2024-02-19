6225772fe50857d2c96e9d417197ae0953656088
Ilya Boyur <ilya.boyur.r@yandex.ru>
2024-02-19 22:29:53 +0300

    Fix examples

--- api_test.py
+++ api_test.py
@@ -6,10 +6,11 @@ def check_python_version():
 
 def check_api():
     from api_client import YandexWeatherAPI
+    from city_repository import CityRepository
 
     CITY_NAME_FOR_TEST = "MOSCOW"
 
-    ywAPI = YandexWeatherAPI()
+    ywAPI = YandexWeatherAPI(city_service=CityRepository())
     response = ywAPI.get_forecasting(CITY_NAME_FOR_TEST)
     response.get("info")
 
