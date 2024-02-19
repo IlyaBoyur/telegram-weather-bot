781bf09deebd819c320987ddd09c9ad9b347e51b
Ilya Boyur <ilya.boyur.r@yandex.ru>
2024-02-19 19:04:58 +0300

    Add pymongo integrations

--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 DEBUG = os.getenv("DEBUG", "False") == "True"
+
 ERR_MESSAGE_TEMPLATE = "Something wrong. Please contact with mentor."
 MIN_MAJOR_PYTHON_VER = 3
 MIN_MINOR_PYTHON_VER = 9
@@ -18,6 +20,10 @@ YANDEX_GEO_API_LANGUAGE = os.getenv("YANDEX_GEO_API_LANGUAGE", "ru_RU")
 
 DATA_ROOT = os.getenv("DATA_ROOT", "./data")
 DATA_FILE = "cities_data_debug.csv" if DEBUG else "cities_data.csv"
+
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017/")
+MONGODB_DBNAME = os.getenv("MONGODB_DBNAME", "weather")
+USE_MONGODB = os.getenv("USE_MONGODB", "False") == "True"
 
 
 def check_python_version():
